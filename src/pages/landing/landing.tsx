bc446860ce339ec4787ba5999b123d1e4a17eba1
aein <mananniaein1234@gmail.com>
2025-09-07 00:26:16 +0330

    add basic framer motion

--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -1,16 +1,24 @@
 import { ButtonMotion } from '../../components';
 import { useNavigate } from 'react-router';
+import { motion } from 'framer-motion';
 export default function Landing() {
   const navigate = useNavigate();
   return (
-    <div className="w-1/3  h-1/3 flex flex-col justify-evenly items-center">
-      <h1 className="text-2xl text-pink-300  font-semibold">Wellcom To FramerMotion Pizza</h1>
-      <ButtonMotion
-        onClick={() => navigate('/base')}
-        className="border  border-pink-300 w-1/2 p-2 rounded-full text-lg text-pink-300"
-      >
-        Create your Pizza
-      </ButtonMotion>
-    </div>
+    <motion.div
+      animate={{ opacity: 1 }}
+      className="w-1/2  h-1/3 flex flex-col justify-around items-center"
+    >
+      <motion.h1 animate={{ fontSize: '35px' }} className=" text-pink-300  font-semibold ">
+        Wellcom To FramerMotion Pizza
+      </motion.h1>
+      <motion.button animate={{ scale: 1.4, rotateX: 0, rotateY: 0 }} className="w-full">
+        <ButtonMotion
+          onClick={() => navigate('/base')}
+          className="border  border-pink-300 w-1/2 p-2 rounded-full text-lg text-pink-300"
+        >
+          Create your Pizza
+        </ButtonMotion>
+      </motion.button>
+    </motion.div>
   );
 }
